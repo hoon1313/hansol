be52c05f248b40602bd3d285179b1beeced1f1e0
hoon1313 <tkdgns757@naver.com>
2022-11-23 00:29:46 +0900

    mainbanner

--- html/js/common.js
+++ html/js/common.js
@@ -2,5 +2,22 @@ $(document).ready(function () { // 문서준비 이벤트 시작
     /* aos */
     AOS.init();
 
+    var mb = new Swiper(".mb", {
+        direction: "vertical",
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        autoplay : {
+            delay : 3000,  // 슬라이드가 머무르는 시간, 5000=5초
+            disableOnInteraction : false,  // false로 설정하면 스와이프 후 자동 재생이 비활성화 되지 않음
+          },
+          loop:true,
+          speed : 2000,
+          effect: 'fade',
+          fadeEffect: {
+            crossFade: true
+        },
+    });
 
 }); // 문서준비 이벤트 끝
